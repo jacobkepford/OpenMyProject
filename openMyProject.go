1fb60ba24c68263484e74a9d7ef97ce268d36026
Jacob Kepford <rtbredge@gmail.com>
2023-10-01 11:00:07 -0500

    Initial project requirements completed, will now open a project

--- openMyProject.go
+++ openMyProject.go
@@ -1,1 +1,44 @@
-package main 
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	projectPath := "C:/Projects"
+
+	folders, err := os.ReadDir(projectPath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	folderSlice := make([]string, len(folders))
+
+	for i, folder := range folders {
+		folderSlice[i] = folder.Name()
+	}
+
+	prompt := promptui.Select{
+		Label: "Select a project",
+		Items: folderSlice,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("code", result)
+	cmd.Dir = projectPath
+	err = cmd.Run()
+
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+}
